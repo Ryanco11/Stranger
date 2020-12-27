f2287e1483a44d78b930ba173fa0d1d9ad653ecd
WangTest-6PM <WangTest-6Pm@gmail.com>
2020-12-27 20:37:46 +0800

    i

--- Stranger/Assets/Script/DevelopScript3.cs
+++ Stranger/Assets/Script/DevelopScript3.cs
@@ -1,18 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DevelopScript3 : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()  3
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+﻿
